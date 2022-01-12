78ce6182f19108a05fcfa8e02ee04c5611d9af89
lgad31vn <lgad31vn@gmail.com>
2022-01-11 23:09:37 -0600

    set up Apollo Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,36 @@
 import { useState } from 'react';
 import './App.css';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  from,
+} from '@apollo/client';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>Hello</header>
-    </div>
-  );
-}
+import { onError } from '@apollo/client/link/error';
+
+const graphQLLink = from([
+  errorLink, // need this to catch errors
+  new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+]);
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, localtion, path }) => {
+      alert(`GraphQL Error: ${message}`);
+    });
+  }
+});
+
+// actual Apollo Client
+const graphQLClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: graphQLLink, // this is the link to backend server
+});
+
+const App = () => {
+  return <ApolloClient client={graphQLClient}></ApolloClient>;
+};
 
 export default App;
