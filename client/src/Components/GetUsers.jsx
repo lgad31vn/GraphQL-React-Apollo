9f7f96d760bd30ecc4e8ad97c159f9885912e72a
lgad31vn <lgad31vn@gmail.com>
2022-01-12 00:02:04 -0600

    added Form component to create new user

--- client/src/Components/GetUsers.jsx
+++ client/src/Components/GetUsers.jsx
@@ -1,10 +1,10 @@
-import React, { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {
   useQuery, // allows us to get the information from the query
   gql, // shorts for graphql
 } from '@apollo/client';
 
-import { LOAD_USERS } from '../GraphQL/Mutations';
+import { LOAD_USERS } from '../GraphQL/Queries';
 
 const GetUsers = () => {
   const {
@@ -13,11 +13,22 @@ const GetUsers = () => {
     data, // actual data from query
   } = useQuery(LOAD_USERS); // works like a react hook
 
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
-    console.log(data);
+    if (data) {
+      setUsers(data.getAllUsers); // getAllUsers is a field in the graphql schema
+    }
   }, [data]); // this hook will run once when the page loads, and re-run every time there is a change in {data}
 
-  return <div></div>;
+  console.log(data);
+  return (
+    <div>
+      {/* {users.map((val) => {
+        return <h2> {val.firstName} </h2>;
+      })} */}
+    </div>
+  );
 };
 
 export default GetUsers;
